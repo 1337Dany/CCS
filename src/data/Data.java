5db6580754266405b0060f77bf90fa43ff8be588
Danial <dmunatayev.dev@gmail.com>
2024-12-28 02:01:31 +0100

    created jar file representation

--- src/data/Data.java
+++ src/data/Data.java
@@ -1,12 +1,6 @@
 package data;
 
 public class Data {
-    //the number of newly connected clients,
-    //• the number of computed requests,
-    //• the numbers of particular requested operations,
-    //• the number of incorrect operations,
-    //• the sum of computed values.
-
     private static int connectedClients = 0;
     private static int computedRequests = 0;
     private static int addOperations = 0;
@@ -16,7 +10,6 @@ public class Data {
     private static int incorrectOperations = 0;
     private static int sumOfResults = 0;
 
-    // Геттеры и сеттеры
     public int getConnectedClients() {
         return connectedClients;
     }
@@ -81,15 +74,14 @@ public class Data {
         sumOfResults += value;
     }
 
-    public String getData(){
-        return "The number of newly connected clients:              " + connectedClients + "\n" +
+    public String getData() {
+        return "The number of newly connected clients:             " + connectedClients + "\n" +
                 "The number of computed requests:                   " + computedRequests + "\n" +
                 "The numbers of particular requested operations: " + "\n" +
                 "Add operations:                                    " + addOperations + "\n" +
                 "Sub operations:                                    " + subOperations + "\n" +
                 "Mul operations:                                    " + mulOperations + "\n" +
                 "Div operations:                                    " + divOperations + "\n" +
-                "The number of incorrect operations:                " + incorrectOperations + "\n" +
-                "The sum of computed values:                        " + sumOfResults;
+                "The number of incorrect operations:                " + incorrectOperations + "\n" + "The sum of computed values:                        " + sumOfResults;
     }
 }
